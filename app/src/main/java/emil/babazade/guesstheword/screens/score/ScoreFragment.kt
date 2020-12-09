5832370665a096f4ad46b12e306e1516a1c6e8da
emil <emilbabazade29@gmail.com>
2020-12-09 15:09:44 +0200

    added scoreViewModel to data binding of score fragment, and some code cleaning

--- app/src/main/java/emil/babazade/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/emil/babazade/guesstheword/screens/score/ScoreFragment.kt
@@ -51,6 +51,7 @@ class ScoreFragment : Fragment() {
 
         viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(requireArguments()).score)
         viewModel = ViewModelProvider(this, viewModelFactory).get(ScoreViewModel::class.java)
+        binding.scoreViewModel = viewModel
 
         viewModel.score.observe(viewLifecycleOwner) {newScore ->
             binding.scoreText.text = newScore.toString()
@@ -62,8 +63,6 @@ class ScoreFragment : Fragment() {
                 viewModel.onPlayAgainComplete()
             }
         }
-
-        binding.playAgainButton.setOnClickListener { viewModel.onPlayAgain() }
 
         return binding.root
     }
